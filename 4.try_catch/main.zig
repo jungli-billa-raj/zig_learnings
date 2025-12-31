d7ec7ab8939c1e4273ef03d0b3410a19b4f062e3
Kumar Raj <rajbhkseries@gmail.com>
2025-12-31 21:09:40 +0530

    4.try_catch finsished

---
+++ 4.try_catch/main.zig
@@ -0,0 +1,31 @@
+const print = @import("std").debug.print;
+
+fn mightFail(x: i32) !i32 {
+    if (x<0) return error.NegativeNumberNotAllowed else return x;
+}
+
+pub fn main() !void {
+    // const correct = mightFail(42) catch |err| {
+    //                                 print("Error occured: {}\n", .{err});
+    //                                 return err;
+    //                             };
+    // print("Result: {d}\n", .{correct});
+    //
+    // const wrong = mightFail(-98) catch |err| {
+    //                                 print("Error occured: {}\n", .{err});
+    //                                 return err;
+    //                             };
+    // print("Result: {d}\n", .{wrong}); // but this doesn't feel right. Instead of returning I want this print stamtement to not run. 
+
+    if (mightFail(42)) |result| {
+        print("{}\n", .{result});
+    } else |err| {
+        print("error occured: {}\n", .{err});
+    }
+
+    if (mightFail(-92)) |result| {
+        print("{}\n", .{result});
+    } else |err| {
+        print("error occured: {}\n", .{err});
+    }
+}
