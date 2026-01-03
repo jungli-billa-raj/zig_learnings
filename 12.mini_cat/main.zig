5427de3c78c9fa7dd14256cf7fd18808bde5d4df
Kumar Raj <rajbhkseries@gmail.com>
2026-01-03 23:46:15 +0530

    mini_cat work needed. Learning argsAlloc() vs argsWithAllocator()

--- 12.mini_cat/main.zig
+++ 12.mini_cat/main.zig
@@ -6,8 +6,12 @@ pub fn main() !void {
     var args = try std.process.argsWithAllocator(allocator);
     defer args.deinit();
 
-    _ = args.next(); // skip the name of the executable
+    _ = args.skip(); // skip the name of the executable
     var i:usize = 1;
+    if (args.next() == null) {
+        std.debug.print("Enter some argument: \n", .{} );
+        return;
+    }
     while (args.next())  |arg| : (i += 1) {
         std.debug.print("{d} : {s}\n", .{i, arg});
     }
